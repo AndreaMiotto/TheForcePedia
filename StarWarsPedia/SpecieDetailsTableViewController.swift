b199368ab5f585352d6f76df965d04bb9e8a8365
Andrea Miotto <andreamiotto@macbook-pro-andrea-miotto.local>
2017-06-25 11:43:21 +0200

    added base for the resto of the details views

---
+++ StarWarsPedia/SpecieDetailsTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SpecieDetailsTableViewController.swift
+//  StarWarsPedia
+//
+//  Created by Andrea Miotto on 25/06/17.
+//  Copyright © 2017 Andrea Miotto. All rights reserved.
+//
+
+import UIKit
+
+class SpecieDetailsTableViewController: UITableViewController {
+    
+    
+}
---
+++ StarWarsPedia/VehicleDetailsTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  VehicleDetailsTableViewController.swift
+//  StarWarsPedia
+//
+//  Created by Andrea Miotto on 25/06/17.
+//  Copyright © 2017 Andrea Miotto. All rights reserved.
+//
+
+import UIKit
+
+class VehicleDetailsTableViewController: UITableViewController {
+    
+    
+}
